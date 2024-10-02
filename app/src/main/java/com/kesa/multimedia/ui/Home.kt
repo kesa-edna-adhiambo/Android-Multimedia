826a8b09540f279faa7df063ada610a682f52a97
kesa-edna-adhiambo <edna@gmail.com>
2024-10-02 20:07:24 +0300

    add activities

--- app/src/main/java/com/kesa/multimedia/ui/Home.kt
+++ app/src/main/java/com/kesa/multimedia/ui/Home.kt
@@ -2,6 +2,7 @@ package com.kesa.multimedia.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.kesa.multimedia.R
 import com.kesa.multimedia.databinding.ActivityHomeBinding
 
@@ -11,6 +12,49 @@ class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_home)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.bnvHome.setOnItemReselectedListener {menuItem ->
+
+            when (menuItem.itemId){
+                R.id.home ->{
+
+                    loadFragment(HomeFragment())
+
+                }
+
+                R.id.games ->{
+
+                    loadFragment(GameFragment())
+
+                }
+
+                R.id.settings ->{
+
+                    loadFragment(SettingsFragment())
+
+                }
+
+                R.id.profile ->{
+
+                    loadFragment(ProfileFragment())
+
+                }
+
+                else -> {
+                    loadFragment(HomeFragment())
+                }
+            }
+        }
+    }
+
+    fun loadFragment(fragment: Fragment):Boolean{
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fcvHome, fragment)
+        fragmentTransaction.commit()
+        return true
     }
 }
